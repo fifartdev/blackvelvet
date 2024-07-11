58414c94286adec56d5503e3034fb25191ee0c65
fifartdev <fifartapp@gmail.com>
2024-07-11 12:57:10 +0300

    3rd version functional

--- app/[id].jsx
+++ app/[id].jsx
@@ -1,15 +1,17 @@
 import { View, Text, SafeAreaView, StyleSheet, ActivityIndicator, ScrollView, Image, Share, Alert } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import { useQuery } from '@tanstack/react-query'
 import { fetchPost, stripHtmlAndDecode } from '../api/services'
 import {Stack} from 'expo-router'
 import Animated, { FadeIn, FadeOut, Easing} from 'react-native-reanimated';
 import ShareIcon from '../components/ShareIcon'
+import HomeIcon from '../components/HomeIcon'
 
 
 const postId = () => {
 
+  const router = useRouter()
   const { id } = useLocalSearchParams()
   const query = useQuery({
     queryKey: ['post'],
@@ -48,7 +50,7 @@ const postId = () => {
 
   return (
       <SafeAreaView>                       
-        <Stack.Screen options={{headerTitle: query?.isFetching ? "Περιμένετε..." : query?.data.title.rendered, headerRight:() => <ShareIcon onPress={()=> shareURL(query?.data.link)}/>, headerBackTitle: 'Πίσω' }} />
+        <Stack.Screen options={{headerTitle: ()=><HomeIcon onPress={()=>router.back()}/>, headerRight:() =>  <ShareIcon onPress={()=> shareURL(query?.data.link)}/>, headerBackTitle: 'Πίσω' }} />
         <Animated.View entering={FadeIn.duration(800).easing(Easing.ease)} exiting={FadeOut}>
           <ScrollView style={styles.post}>
             <Image style={{flex:1, width:'100%', height: 200, backgroundColor: '#c3c3c3'}} contentFit="cover" transition={1000} source={{uri:query?.data.yoast_head_json.og_image[0].url}}/>
